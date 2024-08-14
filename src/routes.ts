bb874cb56cebd5aad806a8da321fc3368b61f780
jjhonny <ojhonny1@hotmail.com>
2024-08-14 10:49:30 -0400

    feat: logging user

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,12 @@
 import { Router } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
-/* router.get("/teste", (req: Request, res: Response) => {
-  return res.json({ ok: true });
-}); */
-
 // -- ROTAS USER --
 router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
 
 export { router };
