151d7a8197a078233b9be54133f2258c901fcfc1
jjhonny <ojhonny1@hotmail.com>
2024-08-22 08:41:18 -0400

    feat: update cuts

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { UpdateUserController } from "./controllers/user/UpdateUserController";
 import { CreateHaircutController } from "./controllers/haircut/CreateHaircutController";
 import { ListHaircutController } from "./controllers/haircut/ListHaircutController";
+import { UpdateHaircutController } from "./controllers/haircut/UpdateHaircutController";
 
 const router = Router();
 
@@ -19,5 +20,6 @@ router.put("/users", isAuthenticated, new UpdateUserController().handle);
 // -- ROTAS HAIRCUT --
 router.post("/haircut", isAuthenticated, new CreateHaircutController().handle);
 router.get("/haircuts", isAuthenticated, new ListHaircutController().handle);
+router.put("/haircut", isAuthenticated, new UpdateHaircutController().handle);
 
 export { router };
